cfff46f97e79b558fdec3e80673429fba987ab7b
Adrian R <109531222+adru75@users.noreply.github.com>
2024-04-30 10:39:44 -0300

    validacion terminada

--- validacion.js
+++ validacion.js
@@ -1,38 +1,43 @@
 /* Tipo de error */
 const tiposError = [
-    "valueMissing",
-    "typeMismatch",
-    "patternMismatch",
-    "tooShort",
-    "customError",
-  ];
+  "valueMissing",
+  "typeMismatch",
+  "patternMismatch",
+  "tooShort",
+  "customError",
+];
 
 /* Mensajes customizados */
 const mensajes = {
-    nombre: {
-      valueMissing: "El campo nombre no puede estar vacío.",
-      patternMismatch: "Por favor, ingrese un nombre válido.",
-      tooShort: "El nombre es muy corto.",
-    },
-    email: {
-      valueMissing: "El campo email no puede estar vacío.",
-      typeMismatch: "Por favor, ingrese un email válido.",
-      tooShort: "Por favor, ingrese un e-mail válido.",
-    },
-    asunto: {
-      valueMissing: "El campo asunto no puede estar vacío.",
-      patternMismatch: "Por favor, ingrese un asunto válido.",
-      tooShort: "El campo no tiene caracteres suficientes.",
-    },
-    mensaje: {
-      valueMissing: "El mensaje no puede estar vacío.",
-      patternMismatch: "Por favor, ingrese un mensaje.",
-      tooShort: "El campo mensaje no tiene caracteres suficientes.",
-    },
- 
-  };
-  
+  nombre: {
+    valueMissing: "El campo nombre no puede estar vacío.",
+    patternMismatch: "Por favor, ingrese un nombre válido.",
+    tooShort: "El nombre es muy corto.",
+  },
+  email: {
+    valueMissing: "El campo email no puede estar vacío.",
+    typeMismatch: "Por favor, ingrese un email válido.",
+    tooShort: "Por favor, ingrese un e-mail válido.",
+  },
+  asunto: {
+    valueMissing: "El campo asunto no puede estar vacío.",
+    patternMismatch: "Por favor, ingrese un asunto válido.",
+    tooShort: "El campo no tiene caracteres suficientes.",
+  },
+  mensaje: {
+    valueMissing: "El mensaje no puede estar vacío.",
+    patternMismatch: "Por favor, ingrese un mensaje.",
+    tooShort: "El campo mensaje no tiene caracteres suficientes.",
+  },
 
+};
+
+const validos = {
+  nombre: false,
+  email: false,
+  asunto: false,
+  mensaje: false,
+};
 
 /* cargo todos los campos que tienen required */
 const camposDeFormulario = document.querySelectorAll("[required]");
@@ -54,23 +59,41 @@ formulario.addEventListener("submit", (e) => {
 });
 */
 
+
+/* desabilito submit */
+document.getElementById("formcontato__botao").disabled = true;
+
+botonEnviar = document.getElementById("formcontato__botao");
+
 /* recorro los campos, y ejecuto funcion verificarCampo al salir de foco*/
+
 camposDeFormulario.forEach((campo) => {
+
+
   campo.addEventListener("blur", () => verificarCampo(campo));
 
   /* desactiva el mensaje de error del navegador */
   campo.addEventListener("invalid", (evento) => evento.preventDefault());
+
+
 });
+
+
 
 function verificarCampo(campo) {
   //console.log("cambio",campo.name); /* me dice el nombre del campo que cambio */
   let mensaje = "";
 
-  if (campo.name =="nombre" && campo.value.length < 3) {
+
+  if (campo.name == "nombre" || campo.name == "asunto" || campo.name == "mensaje") {
+    campo.value = campo.value.trim();
+  }
+
+  if (campo.name == "nombre" && campo.value.length < 3) {
     console.log("nombre muy corto");
   }
 
-  if (campo.name =="mensaje" && campo.value =="") {
+  if (campo.name == "mensaje" && campo.value == "") {
     console.log("mensaje vacio");
   }
 
@@ -82,28 +105,49 @@ function verificarCampo(campo) {
 
   console.log(mensaje);
 
-   /*  console.log(campo.validity); */
+   /* console.log(campo.validity); */
 
-   const mensajeError = campo.parentNode.querySelector(".mensaje-error");
-   const validarInputCheck = campo.checkValidity();
+  const mensajeError = campo.parentNode.querySelector(".mensaje-error");
+  const validarInputCheck = campo.checkValidity();
 
-   
+
   if (!validarInputCheck) {
     mensajeError.textContent = mensaje;
-    campo.style.border = "2px solid red"
+    campo.style.border = "2px solid red";
+    validos[campo.name] = false;
 
   } else {
     mensajeError.textContent = "";
     campo.style.border = "2px solid green";
+    validos[campo.name] = true;
 
   }
   /*
   campo.setCustomValidity("");
-
-
-
-
-
   */
+  console.log(validos);
+
+  if (validos["nombre"] == true && validos["email"] == true && validos["asunto"] == true && validos["mensaje"] == true) {
+    console.log("todos validos");
+    document.getElementById("formcontato__botao").disabled = false;
+
+    botonEnviar.style.border = "1px solid #FFF";
+    botonEnviar.style.backgroundColor = "#2A7AE4";
+    botonEnviar.style.cursor = "pointer";
+    botonEnviar.style.color = "#FFF";
+
+
+  } else {
+    document.getElementById("formcontato__botao").disabled = true;
+    botonEnviar.style.border = "1px solid #272727";
+    botonEnviar.style.backgroundColor = "gray";
+    botonEnviar.style.cursor = "";
+    botonEnviar.style.color = "darkgrey";
+
+
+
+    console.log("alguno incorrecto");
+  }
+
 }
 
